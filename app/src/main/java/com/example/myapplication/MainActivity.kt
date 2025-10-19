a0b6967b52ea96752f846584f0c2091d34ac67f9
melahdal <elahdalmohammed@gmail.com>
2025-10-19 10:34:01 -0400

    Etape 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,10 +47,9 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         // ici ne pas oublier de passser le modif.. en para
         Text(
             text = message,
-            fontSize = 100.sp,
+            fontSize = 85.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center,
-            modifier = modifier
         )
         Text(
             text = from,
@@ -67,7 +67,10 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+
         )
         GreetingText(
             message = message,
